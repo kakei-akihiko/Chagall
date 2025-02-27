1ceab2ec79a0c874e36f5468693d9b4f2189a6ee
掛居明彦 <kakei.akihiko@gmail.com>
2025-02-28 07:30:03 +0900

    プロセスのパス取得不可でエラーメッセージ

--- Chagall/Domain/Features/Windows/Window.cs
+++ Chagall/Domain/Features/Windows/Window.cs
@@ -6,7 +6,13 @@ namespace Chagall.Domain.Features.Windows;
 class Window
 {
     public IntPtr Handle { get; set; }
-    public string? MainModulePath { get; set; }
+    public bool MainModulePathEnabled => _mainModulepath != null;
+    public string? MainModulePath
+    {
+        get => _mainModulepath ?? "（取得できません）";
+        set => _mainModulepath = value;
+    }
+    private string? _mainModulepath = null;
     public int ProcessId { get; set; }
     public Int32Rect Rectangle { get; set; }
     public string? Text { get; set; }
