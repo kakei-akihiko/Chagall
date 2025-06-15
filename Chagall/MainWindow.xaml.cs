41524118e944cfb48d761ce373251ed3b5b306a8
掛居明彦 <kakei.akihiko@gmail.com>
2025-06-16 07:03:12 +0900

    MainWindowの処理を追加

--- Chagall/MainWindow.xaml.cs
+++ Chagall/MainWindow.xaml.cs
@@ -1,12 +1,46 @@
+using Chagall.Usecases;
+using Chagall.Views;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Linq;
 
-namespace Chagall
+namespace Chagall;
+
+public sealed partial class MainWindow : Window
 {
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        Refresh();
+    }
+
+    public MainViewModel ViewModel { get; } = new MainViewModel();
+
+    public void Refresh()
+    {
+        ViewModel.NavigationItems.Clear();
+
+        var items = windowService.GetWindows()
+            .Select(window => new MainNavigationItem()
+            {
+                Tag = window.Handle.ToString(),
+                Title = window.Text,
+                Icon = new SymbolIcon(Symbol.View)
+            });
+
+        foreach (var item in items)
+            ViewModel.NavigationItems.Add(item);
+    }
+
+    private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+    }
+
+    private WindowService windowService = new();
+
+    private void RefreshButton_Click(object sender, RoutedEventArgs e)
+    {
+        Refresh();
     }
 }
